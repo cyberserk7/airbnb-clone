8a6277b775ddb9ff960ece097152d0cf4c14c24c
cyberserk7 <deynilabjo@gmail.cpm>
2023-12-23 15:56:32 +0530

    commit

---
+++ app/components/modals/login-modal.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import useRegisterModal from "@/hooks/use-register-modal";
+import Modal from "./modal";
+import Heading from "../heading";
+import Input from "../inputs/input";
+import toast from "react-hot-toast";
+import Button from "../Button";
+import useLoginModal from "@/hooks/use-login-modal";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+type Props = {};
+
+export default function LoginModal({}: Props) {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    signIn("credentials", {
+      ...data,
+      redirect: false,
+    }).then((callback) => {
+      setIsLoading(false);
+
+      if (callback?.ok) {
+        toast.success("Logged in");
+        router.refresh();
+        loginModal.onClose();
+      }
+
+      if (callback?.error) {
+        toast.error(callback.error);
+      }
+    });
+  };
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn("github")}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row items-center justify-center gap-2">
+          <div>Already have an account?</div>
+          <div className="text-neutral-800 cursor-pointer hover:underline">
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome back" subtitle="Please login to your account." />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Continue"
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+}
---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Container from "../container";
+import Logo from "./logo";
+import Search from "./search";
+import UserMenu from "./user-menu";
+import { SafeUser } from "@/app/types";
+
+interface NavbarProps {
+  currentUser?: SafeUser | null;
+}
+
+const Navbar = ({ currentUser }: NavbarProps) => {
+  return (
+    <div className="fixed w-full bg-white z-10 select-none shadow-sm">
+      <div
+        className="
+                    py-4
+                    border-b-[1px]
+                "
+      >
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
