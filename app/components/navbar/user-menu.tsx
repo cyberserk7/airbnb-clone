41da5eafb9b49baaaa1fe7286ee4f8c3663dcbda
cyberserk7 <deynilabjo@gmail.cpm>
2023-12-26 18:19:57 +0530

    commit

--- app/components/navbar/user-menu.tsx
+++ app/components/navbar/user-menu.tsx
@@ -80,7 +80,10 @@ const UserMenu = ({ currentUser }: UserMenuProps) => {
                   onClick={() => router.push("/reservations")}
                   label="My reservations"
                 />
-                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem
+                  onClick={() => router.push("/properties")}
+                  label="My properties"
+                />
                 <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
                 <MenuItem onClick={() => onSignOut()} label="Logout" />
               </>
